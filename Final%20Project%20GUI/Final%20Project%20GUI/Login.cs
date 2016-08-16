6aa83b6fa3c3532ac1d64f9799de092b3e124ac3
Eran Semo <eransemo@hotmail.com>
2016-08-16 11:52:02 +0300

    Add files via upload

---
+++ Final%20Project%20GUI/Final%20Project%20GUI/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project_GUI
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Final%20Project%20GUI/Final%20Project%20GUI/SystemAdminGUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project_GUI
+{
+    public partial class SystemAdminGUI : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        public SystemAdminGUI()
+        {
+            InitializeComponent();
+        }
+
+  
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            UsersManagment users = new UsersManagment();
+            users.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TopologyManagment topo = new TopologyManagment();
+            topo.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
